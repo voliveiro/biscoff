8bf6e1761d5f32740fc0722ad4cc765c8135d46b
Vernie <vernie.oliveiro@gmail.com>
2021-01-28 19:59:32 +0800

    added show and index routes

--- server.js
+++ server.js
@@ -4,9 +4,31 @@ const app = express()
 const ejs = require('ejs');
 const data = require('./models/data.js')
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// require('./router')(app);
+
 app.get('/', (req, res) => {
-    res.send("Connected!")
+    res.send(data)
 })
+
+app.get('/index', (req, res) => {
+    res.render ('index.ejs', {
+        bakedgoods : data
+    })
+
+})
+
+app.get('/show/:index/', (req, res) => {
+    const indexno = parseInt(req.params.index); 
+    res.render('show.ejs', {
+        bakedgood : data[indexno]
+
+    })
+})
+
 app.listen (process.env.PORT, ()=> {
     console.log ('app is listening at port 3000')
 })
